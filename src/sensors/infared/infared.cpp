35a169c450e4fef087e7bd6ad57e2e2c68272cfa
Nuwan Jaliyagoda <nuwanjaliyagoda@gmail.com>
2023-03-21 17:13:53 +0530

    Changes to be committed: 	modified:   src/sensors/infared/infared.cpp 	modified:   src/sensors/infared/infared.h

--- src/sensors/infared/infared.cpp
+++ src/sensors/infared/infared.cpp
@@ -2,12 +2,33 @@
 
 SW_Infared ir;
 
+// NOTE: These code block is originally placed in setup.ino, and moved to here
+// Functionality isn't checked yet
+
+// Interrupts for Remote Rx Event
+extern "C" void irReceive_0(uint32_t *data, size_t len, void *arg)
+{
+    parseRmtData((rmt_data_t *)data, len, 0);
+}
+extern void irReceive_1(uint32_t *data, size_t len, void *arg)
+{
+    parseRmtData((rmt_data_t *)data, len, 1);
+}
+extern void irReceive_2(uint32_t *data, size_t len, void *arg)
+{
+    parseRmtData((rmt_data_t *)data, len, 2);
+}
+extern void irReceive_3(uint32_t *data, size_t len, void *arg)
+{
+    parseRmtData((rmt_data_t *)data, len, 3);
+}
+
 void beginInfared()
 {
-    rmtRead(ir.rmt_rx[0], irReceive_0);
-    rmtRead(ir.rmt_rx[1], irReceive_1);
-    rmtRead(ir.rmt_rx[2], irReceive_2);
-    rmtRead(ir.rmt_rx[3], irReceive_3);
+    rmtRead(ir.rmt_rx[0], irReceive_0, NULL);
+    rmtRead(ir.rmt_rx[1], irReceive_1, NULL);
+    rmtRead(ir.rmt_rx[2], irReceive_2, NULL);
+    rmtRead(ir.rmt_rx[3], irReceive_3, NULL);
     ir.begin();
 }
 
@@ -22,25 +43,4 @@ void parseRmtData(rmt_data_t *items, size_t len, uint8_t rId)
 void send(int value)
 {
     ir.sendWaveform(value);
-}
-
-// NOTE: These code block is originally placed in setup.ino, and moved to here
-// Functionality isn't checked yet
-
-// Interrupts for Remote Rx Event
-extern "C" void irReceive_0(uint32_t *data, size_t len)
-{
-    parseRmtData((rmt_data_t *)data, len, 0);
-}
-extern "C" void irReceive_1(uint32_t *data, size_t len)
-{
-    parseRmtData((rmt_data_t *)data, len, 1);
-}
-extern "C" void irReceive_2(uint32_t *data, size_t len)
-{
-    parseRmtData((rmt_data_t *)data, len, 2);
-}
-extern "C" void irReceive_3(uint32_t *data, size_t len)
-{
-    parseRmtData((rmt_data_t *)data, len, 3);
 }
